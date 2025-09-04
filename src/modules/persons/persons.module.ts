2f6cc2ac24ffef1b22131aac56d07f666ae60032
Ayman Al-Khatib <king1aeman@gmail.com>
2025-09-04 18:23:40 +0300

    refactor(persons): simplify PersonsModule by removing file validation options
    - Removed file validation options from PersonsModule to streamline the module's configuration.
    - Retained the PersonsService in the providers array for continued functionality.
    - Organized import statements for improved clarity and maintainability.

--- src/modules/persons/persons.module.ts
+++ src/modules/persons/persons.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DEFAULT_FILE_VALIDATION_OPTIONS } from '../../shared/modules/app-storage/constants/file-validation.constants.ts';
-import { STORAGE_CONSTANTS } from '../../shared/modules/app-storage/constants/storage.constants';
-import { FileValidationOptions } from '../../shared/modules/app-storage/types';
 import { DropdownsModule } from '../dropdowns/dropdowns.module';
 import { PersonController } from './controllers/person.controller';
 import { Person } from './entities/person.entity';
@@ -11,18 +8,7 @@ import { PersonsService } from './services/persons.service';
 @Module({
   imports: [DropdownsModule, TypeOrmModule.forFeature([Person])],
   controllers: [PersonController],
-  providers: [
-    {
-      provide: STORAGE_CONSTANTS.FILE_VALIDATION_CONFIG,
-      useValue: {
-        ...DEFAULT_FILE_VALIDATION_OPTIONS,
-        isFileRequired: false,
-        globalMaxFileSize: '2MB',
-      } as FileValidationOptions,
-    },
-
-    PersonsService,
-  ],
+  providers: [PersonsService],
   exports: [PersonsService],
 })
 export class PersonsModule {}
